a49ca5e029eaa07f92649d41e39328ccf8785e82
Damon Oehlman <damon.oehlman@gmail.com>
2014-06-25 21:02:42 +1000

    Ensure an id for the created item is returned

--- tests/post-create.js
+++ tests/post-create.js
@@ -2,11 +2,15 @@ module.exports = function(url, test) {
   var request = require('supertest')(url);
 
   test('can create a new post', function(t) {
-    t.plan(1);
+    t.plan(2);
 
     request
       .post('/post')
       .send({ title: 'My experience with jsonapi' })
-      .expect(201, t.ifError);
+      .expect(201)
+      .end(function(err, res) {
+        t.ifError(err);
+        t.ok(res.body && res.body.id, 'id generated');
+      });
   });
 };
