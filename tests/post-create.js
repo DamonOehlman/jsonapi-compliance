6bf3cf22aec4288c949a2af2a175e143e9d41432
Damon Oehlman <damon.oehlman@gmail.com>
2014-06-30 21:41:40 +1000

    Expand tests to check for multiple post get

--- tests/post-create.js
+++ tests/post-create.js
@@ -1,7 +1,9 @@
+var pluck = require('whisk/pluck');
+
 module.exports = function(url, test) {
   var post = require('./helpers/post')(url);
   var get = require('./helpers/get')(url);
-  var postId;
+  var postIds = [];
 
   test('post data with no "posts" array fails', function(t) {
     t.plan(2);
@@ -14,21 +16,45 @@ module.exports = function(url, test) {
   test('can create a new post', function(t) {
     t.plan(3);
     post('/posts', { posts: [{ title: 'My experience with jsonapi' }] }, function(err, res, body) {
-      postId = body && body.posts && body.posts[0] && body.posts[0].id;
+      postIds = (body && body.posts || []).map(pluck('id'));
 
       t.ifError(err);
       t.equal(res.statusCode, 201);
-      t.ok(postId, 'id generated');
+      t.ok(postIds[0], 'id generated');
     });
   });
 
   test('can get the newly created post', function(t) {
     t.plan(3);
 
-    get('/posts/' + postId, function(err, res, body) {
+    get('/posts/' + postIds[0], function(err, res, body) {
       t.ifError(err);
       t.equal(res.statusCode, 200);
       t.ok(Array.isArray(body && body.posts), 'got posts');
+    });
+  });
+
+  test('can create a second post', function(t) {
+    t.plan(3);
+    post('/posts', { posts: [{ title: 'Another post' }] }, function(err, res, body) {
+      postIds = postIds.concat((body && body.posts || []).map(pluck('id')));
+
+      t.ifError(err);
+      t.equal(res.statusCode, 201);
+      t.ok(postIds[1], 'id generated');
+    });
+  });
+
+  test('can get both posts', function(t) {
+    t.plan(3);
+
+    get('/posts/' + postIds.join(','), function(err, res, body) {
+      t.ifError(err);
+      t.equal(res.statusCode, 200);
+      t.deepEqual((body && body.posts || []).map(pluck('title')), [
+        'My experience with jsonapi',
+        'Another post'
+      ]);
     });
   });
 };
