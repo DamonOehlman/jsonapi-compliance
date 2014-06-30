7a9850566f114d032bc87dec86eb09b70e5de3cd
Damon Oehlman <damon.oehlman@gmail.com>
2014-06-30 20:31:27 +1000

    get the post after creating the post

--- tests/post-create.js
+++ tests/post-create.js
@@ -1,12 +1,26 @@
 module.exports = function(url, test) {
   var post = require('./helpers/post')(url);
+  var get = require('./helpers/get')(url);
+  var postId;
 
   test('can create a new post', function(t) {
     t.plan(3);
-    post('/post', { title: 'My experience with jsonapi' }, function(err, res, body) {
+    post('/posts', { title: 'My experience with jsonapi' }, function(err, res, body) {
+      postId = body && body.id;
+
       t.ifError(err);
       t.equal(res.statusCode, 201);
-      t.ok(body && body.id, 'id generated');
+      t.ok(postId, 'id generated');
+    });
+  });
+
+  test('can get the newly created post', function(t) {
+    t.plan(3);
+
+    get('/posts/' + postId, function(err, res, body) {
+      t.ifError(err);
+      t.equal(res.statusCode, 200);
+      t.equal(body && body.title, 'My experience with jsonapi');
     });
   });
 };
